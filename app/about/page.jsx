a3142a5131b40d0c45378e1daaf6cfd5cde1cb0f
Dev-webp <srichain18@gmail.com>
2024-11-02 18:52:52 +0530

    page-about

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -1,48 +1,45 @@
-"use client"
+"use client";
 
 import React from 'react';
-import './AboutUs.css';
+import './page-about.css';
 import { features } from '../../src/utils/data';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
 import { containerVariants, desVariants, tagVariants, titleVariants } from '../../src/utils/animation';
 
-
 const AboutUs = () => {
   return (
-    <div className='au-wrapper'>
-        <div className='container'>
-            <div className='au-container'>
+    <div className='about-wrapper'>
+        <div className='about-container'>
+            <div className='about-support-section'>
                 <motion.div 
                  initial="offscreen"
                  whileInView={"onscreen"}
                  variants={containerVariants(0.3)}
-                className='au-support'>
+                className='about-support'>
                 <div>
-                    <span className='te'>We Partner to<span className='de'>Deliver</span></span>
+                    <span className='about-title'>We Partner to<span className='about-highlight'>Deliver</span></span>
                     </div>
 
-                    <div className='text-a'>
-                        <span className='text'>
+                    <div className='about-text-content'>
+                        <span className='about-text'>
                         KALVEN IT GROUP is a customer-centric transformational global IT solutions and services partner. 
                         </span>
-                        <span className='text'>
+                        <span className='about-text'>
                         We have a proven track record in delivering battle-tested consulting, technology, and outsourcing services to address our clients’ evolving and complex digital transformation challenges. 
                         </span>
-                        <span className='text'>
+                        <span className='about-text'>
                         We harness business-centric, innovative frameworks and solutions to help clients achieve unprecedented performance levels and revenue growth at optimized costs. 
                         </span>
-                        <span className='text'>
+                        <span className='about-text'>
                         Recognized as one of the fastest-growing IT services firms globally, YASH has complemented its robust organic growth with strategic ‘tuck-in’ acquisitions.
                         </span>
                     </div>
                 </motion.div>
-
             </div>
         </div>
-        
     </div>
   )
 }
 
-export default AboutUs
+export default AboutUs;
